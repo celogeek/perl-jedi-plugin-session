5e9809ed8b56c61da0bed7b4a0ca7c8a3845d9e2
celogeek <me@celogeek.com>
2013-12-24 18:24:01 +0100

    fix pod

--- lib/Jedi/Plugin/Session.pm
+++ lib/Jedi/Plugin/Session.pm
@@ -24,7 +24,7 @@ sub import {
 __END__
 =head1 DESCRIPTION
 
-This plugin add to the L<Jedi::Request> two method : session_get and session_set
+This plugin add to the L<Jedi::Request> two methods : B<session_get> and B<session_set>
 
 A secure UUID is generated on all get/post request, and if the cookie jedi_session is missing, then it will automatically
 create it to keep the same UUID between each request.
@@ -39,9 +39,9 @@ The session is very specific to an app, different app in the same L<Jedi> instan
  use Jedi::App;
  use Jedi::Plugin::Session;
  use JSON;
-
  sub jedi_app {
   my ($app) = @_;
+
   $app->get('/set_session', sub {
     my ($app, $request, $response) = @_;
     my $session = $request->session_get // {};
@@ -49,7 +49,7 @@ The session is very specific to an app, different app in the same L<Jedi> instan
     $request->session_set($session);
     $response->status(200);
     $response->body('session set !')
-  })
+  });
  
   $app->get('/get_session', sub {
     my ($app, $request, $response) = @_;
@@ -57,14 +57,16 @@ The session is very specific to an app, different app in the same L<Jedi> instan
     $response->status(200);
     $response->body(defined $session ? encode_json($session) : 'session not defined !');
   })
-
  }
+ 1;
 
-=head1 EXPIRATION
+=head1 LIMITATION
 
 The session is keep in memory and serialized. You can't save CODE or unserializable object in the session.
 
 The default expiration is '3 hours'. The cookie with a part of the UUID is keep for 2 years and sent only once.
+
+=head1 EXPIRATION
 
 To change the default expiration for your app, you can use the configuration like this :
 
@@ -76,4 +78,3 @@ Check L<CHI> for the possible value of the expiration.
 If you need to set the full expiration time again, you can just set again the session :
 
  $request->session_set($request->session_get // {});
- 
