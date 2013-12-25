35b10e88d7b94319204d77f2889706097ebc2655
celogeek <me@celogeek.com>
2013-12-25 17:09:14 +0100

    fix config session

--- lib/Jedi/Plugin/Session/Role/Redis.pm
+++ lib/Jedi/Plugin/Session/Role/Redis.pm
@@ -13,8 +13,8 @@ sub _build__jedi_session {
     my ($self) = @_;
     my $class = ref $self;
     my $expires_in = $self->jedi_config->{$class}{session}{expiration} // '3 hours';
-    my $redis_config = $self->jedi_config->{$class}{redis}{config};
-    my $redis_prefix = $self->jedi_config->{$class}{redis}{prefix};
+    my $redis_config = $self->jedi_config->{$class}{session}{redis}{config};
+    my $redis_prefix = $self->jedi_config->{$class}{session}{redis}{prefix};
     return Jedi::Plugin::Session::Backend::Redis->new(
       config => $redis_config,
       expires_in => $expires_in,
