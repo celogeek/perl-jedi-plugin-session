407b53a34019511b13e64965bf3fb8b270640779
celogeek <me@celogeek.com>
2013-12-25 15:29:54 +0100

    add pod

--- lib/Jedi/Plugin/Session/Role/Redis.pm
+++ lib/Jedi/Plugin/Session/Role/Redis.pm
@@ -15,7 +15,11 @@ sub _build__jedi_session {
     my $expires_in = $self->jedi_config->{$class}{session_expiration} // '3 hours';
     my $redis_config = $self->jedi_config->{$class}{redis_config};
     my $redis_prefix = $self->jedi_config->{$class}{redis_prefix};
-    return Jedi::Plugin::Session::Backend::Redis->new(config => $redis_config, expires_in => $expires_in, prefix => $redis_prefix);
+    return Jedi::Plugin::Session::Backend::Redis->new(
+      config => $redis_config,
+      expires_in => $expires_in,
+      prefix => $redis_prefix
+    );
 }
 
 1;
